b07ad45418cc13dfa46a9567e2b7fa827fc77b39
Dhanush Kiran K <dhanuu08@gmail.com>
2020-05-24 13:30:48 +0530

    added store

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App.js';
+import { configureStore } from './store';
+const store = configureStore();
+console.log(store.getState());
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
